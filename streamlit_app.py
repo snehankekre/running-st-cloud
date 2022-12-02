43065426ba52dcc957e3744cc78d239d8fc458d1
Snehan Kekre <snehan.minerva@gmail.com>
2022-12-02 17:39:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
-from typing import Dict, Optional
-
-from streamlit.runtime.metrics_util import gather_metrics
 from streamlit.runtime.scriptrunner import get_script_run_ctx
 from streamlit.web.server.browser_websocket_handler import BrowserWebSocketHandler
 
@@ -8,7 +5,7 @@ import streamlit as st
 from streamlit import config, runtime, url_util
 
 
-def is_running_on_cloud() -> Optional[Dict[str, str]]:
+def is_running_on_cloud() -> bool:
     """Return a copy of the HTTP request headers for the current session's
     WebSocket connection. If there's no active session, return None instead.
     Raise an error if the server is not running.
